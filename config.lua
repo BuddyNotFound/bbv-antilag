e81011c2099b0721ec53bac6fa612ed6c3ca70dd
BuddyBoyVilla <74051918+BuddyNotFound@users.noreply.github.com>
2023-08-06 19:46:11 +0300

    sql

--- config.lua
+++ config.lua
@@ -1,17 +1,17 @@
 Config = {}
 
-QBCore = exports['qb-core']:GetCoreObject()  -- uncomment if you use QBCore
+-- QBCore = exports['qb-core']:GetCoreObject()  -- uncomment if you use QBCore
 -- ESX = exports["es_extended"]:getSharedObject() -- uncomment if you use ESX
 
 Config.Settings = {
-    Framework = "QB", -- QB/ESX/ST (ST - Standalone) used for the notify
+    Framework = "ST", -- QB/ESX/ST (ST - Standalone) used for the notify
     ParticleUsed = 'veh_xs_vehicle_mods', -- Particle used (if you are using colors from : https://forum.cfx.re/t/paid-nitro-system-changeable-colors-much-more-qb-esx-standalone/5156013)
     RPM = 0.65, -- the RPM when the pop/antilag gets triggered
     InstallDist = 2.5,
     WebHook = "",
     Commands = {
         Enabled = true, -- if set to true all 'Config.Items' will also be created as commands (used when framework is Standalone) can also be used with frameworks like admin commands
-        Permissions = false, -- if set to true only "allowed" user will be ablo to use them
+        Permissions = true, -- if set to true only "allowed" user will be ablo to use them
         Allowed = { -- users allowed to use commands
             'steam:11000013b******', -- Buddy 
             'steam:steamid',
---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `bbv_antilag` (
+  `plate` varchar(255) DEFAULT NULL,
+  `antilag` int(11) DEFAULT NULL,
+  `installer` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
